7f73289ccbbac8e34efb167644c982203cd46c9b
Levente Csiszar <cls1mc@bosch.com>
2022-06-14 15:18:38 +0200

    initial commit

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,252 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control" :class="{ invalid: !firstName.isValid }">
+      <label for="firstname">Firstname</label>
+      <input
+        type="text"
+        id="firstname"
+        v-model.trim="firstName.val"
+        @blur="clearValidity('firstName')"
+      />
+      <span v-if="!firstName.isValid">Firstname must not be empty.</span>
+    </div>
+    <div class="form-control" :class="{ invalid: !lastName.isValid }">
+      <label for="lastname">Lastname</label>
+      <input
+        type="text"
+        id="lastname"
+        v-model.trim="lastName.val"
+        @blur="clearValidity('lastName')"
+      />
+      <span v-if="!lastName.isValid">Lastname must not be empty.</span>
+    </div>
+    <div class="form-control" :class="{ invalid: !description.isValid }">
+      <label for="description">Description</label>
+      <textarea
+        id="description"
+        cols="30"
+        rows="5"
+        v-model.trim="description.val"
+        @blur="clearValidity('description')"
+      ></textarea>
+      <span v-if="!description.isValid">Description must not be empty.</span>
+    </div>
+    <div class="form-control" :class="{ invalid: !rate.isValid }">
+      <label for="rate">Hourly Rate</label>
+      <input
+        type="number"
+        id="rate"
+        v-model.number="rate.val"
+        @blur="clearValidity('rate')"
+      />
+      <span v-if="!rate.isValid">Rate must be greater than 0.</span>
+    </div>
+    <div class="form-control" :class="{ invalid: !areas.isValid }">
+      <h3>Areas of Expertise</h3>
+      <div>
+        <input
+          type="checkbox"
+          id="frontend"
+          value="frontend"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
+        <label for="frontend">Frontend Development</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="backend"
+          value="backend"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
+        <label for="backend">Backend Development</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="career"
+          value="career"
+          v-model="areas.val"
+          @blur="clearValidity('areas')"
+        />
+        <label for="career">Career Advisory</label>
+      </div>
+      <span v-if="!areas.isValid"
+        >At least one expertise must be selected.</span
+      >
+    </div>
+    <p v-if="!formIsValid">Please fix the above errors and submit again!</p>
+    <base-button>Register</base-button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['save-data'],
+  data() {
+    return {
+      firstName: {
+        val: '',
+        isValid: true,
+      },
+      lastName: {
+        val: '',
+        isValid: true,
+      },
+      description: {
+        val: '',
+        isValid: true,
+      },
+      rate: {
+        val: null,
+        isValid: true,
+      },
+      areas: {
+        val: [],
+        isValid: true,
+      },
+      formIsValid: true,
+      formIsAlreadySubmitted: false,
+    };
+  },
+  methods: {
+    validateForm() {
+      this.formIsValid = true;
+      this.validateFirstName();
+      this.validateLastName();
+      this.validateDescription();
+      this.validateRate();
+      this.validateAreas();
+    },
+    validateFirstName() {
+      if (this.firstName.val === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    validateLastName() {
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    validateDescription() {
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    validateRate() {
+      if (!this.rate.val || this.rate.val < 0) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    validateAreas() {
+      if (this.areas.val.length === 0) {
+        this.areas.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    clearValidity(input) {
+      if (!this.formIsAlreadySubmitted) return;
+      this[input].isValid = true;
+      switch (input) {
+        case 'firstName':
+          this.validateFirstName();
+          break;
+        case 'lastName':
+          this.validateLastName();
+          break;
+        case 'description':
+          this.validateDescription();
+          break;
+        case 'rate':
+          this.validateRate();
+          break;
+        case 'areas':
+          this.validateAreas();
+          break;
+      }
+    },
+    submitForm() {
+      this.formIsAlreadySubmitted = true;
+      this.validateForm();
+
+      if (!this.formIsValid) {
+        return;
+      }
+
+      const formData = {
+        first: this.firstName.val,
+        last: this.lastName.val,
+        desc: this.description.val,
+        rate: this.rate.val,
+        areas: this.areas.val,
+      };
+
+      this.$emit('save-data', formData);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type='checkbox'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type='checkbox']:focus {
+  outline: #3d008d solid 1px;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.invalid span,
+p {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+</style>
---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <base-dialog :show="!!error" title="An error occured" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="email" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long).
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+      isLoading: false,
+      error: null,
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Login';
+      } else {
+        return 'Sign Up';
+      }
+    },
+    switchModeButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Signup instead';
+      } else {
+        return 'Login instead';
+      }
+    },
+  },
+  methods: {
+    async submitForm() {
+      this.formIsValid = true;
+      if (this.email === '' || this.password.length < 6) {
+        this.formIsValid = false;
+        return;
+      }
+
+      this.isLoading = true;
+
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
+
+      //send http request...
+      try {
+        if (this.mode === 'login') {
+          await this.$store.dispatch('login', actionPayload);
+        } else {
+          await this.$store.dispatch('signup', actionPayload);
+        }
+        if (this.$route.query.redirect) {
+          if (this.$store.getters['coaches/isCoach']) {
+            this.$router.replace('/coaches');
+          } else {
+            this.$router.replace('/' + this.$route.query.redirect);
+          }
+        } else {
+          this.$router.replace('/coaches');
+        }
+        // const redirectUrl = '/' + (this.$route.query.redirect || 'coaches');
+        // this.$router.replace(redirectUrl);
+      } catch (err) {
+        this.error = err.message || 'Failed to authecticate, try later.';
+      }
+
+      this.isLoading = false;
+    },
+    switchAuthMode() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+</style>
